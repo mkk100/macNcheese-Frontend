2729e2813babe796a19d9fe7babfea214ea3eabd
mkk100 <tommychimes2003@gmail.com>
2024-08-24 20:16:18 -0700

    get works

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
         GET
       </a>
       <a href="/Post" className="h-16 w-32 bg-[#ffbd88] transition-opacity ease-in duration-200 opacity-100 hover:opacity-70 text-center flex items-center justify-center text-white">
-        POST
+        Add Recipe
       </a>
       <a href="/Put" className="h-16 w-32 bg-[#ffbd88] transition-opacity ease-in duration-200 opacity-100 hover:opacity-70 text-center flex items-center justify-center text-white">
         PUT
--- src/app/Post/page.tsx
+++ src/app/Post/page.tsx
@@ -1,10 +1,36 @@
+'use client'
+import { FormEvent } from "react";
 import { Navbar } from "../Navbar";
+import axios from "axios";
 
 export default function Put() {
+  async function onSubmit(event:FormEvent<HTMLFormElement>){
+    event.preventDefault()
+    console.log(event.currentTarget.value)
+    const formData = new FormData(event.currentTarget)
+  }
   return (
     <div>
       <Navbar />
-      Post
+      <form className="flex flex-col items-center mt-16" onSubmit={onSubmit}>
+      <input
+          type="text"
+          name="name"
+          placeholder="Recipe Name"
+          className="px-4 py-2 text-lg border border-gray-300 rounded mr-2"
+        /><br/>
+          <textarea
+          name="name"
+          placeholder="Ingredients" rows="5" cols="35"
+          className="px-4 py-2 text-lg border border-gray-300 rounded mr-2"
+        /><br/>
+          <textarea
+          name="name"
+          placeholder="Directions" rows="15" cols="35"
+          className="px-4 py-2 text-lg border border-gray-300 rounded mr-2"
+        /><br/>
+        <button className="bg-[#ffbd88] text-white w-32 h-12 rounded-lg">Add</button>
+      </form>
     </div>
   );
 }
