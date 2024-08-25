4161cf110cc0f25abd4288582435877b32bf71df
mkk100 <tommychimes2003@gmail.com>
2024-08-25 09:50:30 -0700

    get and add and put work

--- src/app/Put/page.tsx
+++ src/app/Put/page.tsx
@@ -1,10 +1,74 @@
+"use client"
+import { FormEvent, useEffect, useState } from "react";
 import { Navbar } from "../Navbar";
+import axios from "axios";
+import router, { useRouter } from "next/navigation";
 
 export default function Put() {
+  const [name,setName] = useState("")
+  const [ingredients,setIngredients] = useState("")
+  const [directions,setDirections] = useState("")
+  const [redirect, setRedirect] = useState(false)
+  const router = useRouter();
+
+  useEffect(()=>{
+    if (redirect){
+      router.push('/')
+    }
+  },[redirect])
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const recipeData = {
+      name: name,
+      ingredients: ingredients,
+      directions: directions,
+    };
+    console.log(recipeData);
+    await axios
+      .put("http://localhost:8080/api/recipes/" + name, {
+        headers: { "Content-Type": "application/json" },
+        data: recipeData,
+      })
+      .then((res) => {
+        setRedirect(true);
+        alert("Recipe Added Successfully!")
+      });
+  }
+
+
   return (
     <div>
       <Navbar />
-      Put
+      <form className="flex flex-col items-center mt-16" onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Recipe Name"
+          className="px-4 py-2 text-lg border border-gray-300 rounded mr-2"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <textarea
+          name="name"
+          placeholder="Ingredients"
+          rows="5"
+          cols="35"
+          className="px-4 py-2 text-lg border border-gray-300 rounded mr-2"
+          onChange={(e) => setIngredients(e.target.value)}
+        />
+        <br />
+        <textarea
+          name="name"
+          placeholder="Directions"
+          rows="15"
+          cols="35"
+          className="px-4 py-2 text-lg border border-gray-300 rounded mr-2"
+          onChange={(e) => setDirections(e.target.value)}
+        />
+        <br />
+        <button className="bg-[#ffbd88] text-white w-32 h-12 rounded-lg">
+          Update
+        </button>
+        </form>
     </div>
   );
 }
